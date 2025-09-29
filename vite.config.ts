73b831ea9359f7e5e868f95203b51a9c7579fa75
afgto79 <cafoucthman@hotmail.com>
2025-09-29 18:59:31 +0200

    fix: update Vite configuration for Vercel

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,13 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+﻿import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
-  test: {
-    environment: 'jsdom'
+  build: {
+    outDir: 'dist',
+    emptyOutDir: true
+  },
+  server: {
+    port: 3000
   }
-})
+});
